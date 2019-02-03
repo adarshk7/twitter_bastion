41a1a47359ed9811a31b8a91ae73c98bd33c6882
Adarsh Krishnan <adarshk7@gmail.com>
2019-02-03 21:13:14 +0200

    Add base application settings

--- twitter_bastion/settings/base.py
+++ twitter_bastion/settings/base.py
@@ -1,5 +1,25 @@
 import os
 
 
+# Neo4j settings
+
 NEO4J_USER = os.environ.get('NEO4J_USER', 'neo4j')
 NEO4J_PASSWORD = os.environ.get('NEO4J_PASSWORD', 'neo4j')
+
+NEO4J_BOLT = os.environ.get('NEO4J_BOLT', True)
+NEO4J_SERVER_ADDRESS = os.environ.get('NEO4J_SERVER_ADDRESS', 'localhost')
+BOLT_PORT = os.environ.get('BOLT_PORT', 7687)
+
+
+# Twitter API credentials
+
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
+
+
+# Target hashtag
+
+TARGET_HASHTAG = os.environ['TARGET_HASHTAG']
