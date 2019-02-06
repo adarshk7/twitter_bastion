d9d03b644d3e4f5459cc2a83e70e91879843b8ad
Adarsh Krishnan <adarshk7@gmail.com>
2019-02-06 22:49:38 +0200

    Add tweet count compare functions over time windows

--- twitter_bastion/services.py
+++ twitter_bastion/services.py
@@ -1,6 +1,11 @@
+from datetime import datetime, timedelta
+
 from twitter_bastion.extensions import db
 from twitter_bastion.models import Tweet, TwitterUser, Hashtag
-from twitter_bastion.utils import format_twitter_datetime_to_isoformat
+from twitter_bastion.utils import (
+    format_twitter_datetime_to_isoformat,
+    round_datetime_to_nearest_minutes,
+)
 
 
 def add_tweet_and_related_data(data):
@@ -44,3 +49,60 @@ def add_hashtag(data):
     hashtag = Hashtag()
     hashtag.value = data['text']
     return hashtag
+
+
+def get_tweet_count_for_time_inteval(start_time, end_time):
+    return db.graph.evaluate(
+        f"MATCH (t:Tweet) "
+        f"WHERE t.created_at >= '{start_time.isoformat()}' AND "
+        f"      t.created_at < '{end_time.isoformat()}' AND"
+        f"      NOT t:Archived "
+        f"RETURN count(*)"
+    )
+
+
+def get_tweet_count_percent_difference_between_time_windows(
+    window_1_start_time,
+    window_1_end_time,
+    window_2_start_time,
+    window_2_end_time,
+):
+    window_1_count = get_tweet_count_for_time_inteval(
+        window_1_start_time, window_1_end_time
+    )
+    window_2_count = get_tweet_count_for_time_inteval(
+        window_2_start_time, window_2_end_time
+    )
+    if not window_1_count or not window_2_count:
+        return 0
+    return 100 * (float(window_2_count - window_1_count) / window_1_count)
+
+
+def get_tweet_count_percent_difference_for_subsequent_time_windows(
+    time_window_length_in_minutes, time_window_interval_in_minutes
+):
+    window_length_timedelta = timedelta(minutes=time_window_length_in_minutes)
+    window_interval_timedelta = timedelta(
+        minutes=time_window_interval_in_minutes
+    )
+    rounded_current_time = round_datetime_to_nearest_minutes(
+        datetime.utcnow(), minutes=time_window_length_in_minutes
+    )
+    current_window_start_time = (
+        rounded_current_time - window_length_timedelta
+    )
+    current_window_end_time = rounded_current_time
+    previous_window_start_time = (
+        current_window_start_time - window_interval_timedelta
+    )
+    previous_window_end_time = (
+        current_window_end_time - window_interval_timedelta
+    )
+    return (
+        get_tweet_count_percent_difference_between_time_windows(
+            previous_window_start_time,
+            previous_window_end_time,
+            current_window_start_time,
+            current_window_end_time,
+        )
+    )
--- twitter_bastion/utils.py
+++ twitter_bastion/utils.py
@@ -3,3 +3,13 @@ from datetime import datetime
 
 def format_twitter_datetime_to_isoformat(value):
     return datetime.strptime(value, '%a %b %d %H:%M:%S %z %Y').isoformat()
+
+
+def round_datetime_to_nearest_minutes(value, minutes=1):
+    return datetime(
+        value.year,
+        value.month,
+        value.day,
+        value.hour,
+        value.minute - (value.minute % minutes)
+    )
