d652baf926c786aae17811c979e1bc43b577692e
Adarsh Krishnan <adarshk7@gmail.com>
2019-02-06 22:51:41 +0200

    Add task for monitoring tweets and fix archiving task

--- twitter_bastion/tasks.py
+++ twitter_bastion/tasks.py
@@ -1,11 +1,16 @@
 import time
+from datetime import timedelta
 
 from flask import current_app
+from sentry_sdk import capture_message
 
 from twitter_bastion.celery import celery_app
 from twitter_bastion.extensions import db, twitter
 from twitter_bastion.models import Tweet
-from twitter_bastion.services import add_tweet_and_related_data
+from twitter_bastion.services import (
+    add_tweet_and_related_data,
+    get_tweet_count_percent_difference_for_subsequent_time_windows,
+)
 
 
 @celery_app.task
@@ -18,17 +23,43 @@ def stream_twitter_data():
 @celery_app.task
 def archive_old_tweets():
     max_unarchived_tweets = current_app.config['MAX_UNARCHIVED_TWEETS']
-    archiving_interval = current_app.config['ARCHIVING_INTERVAL']
 
-    while True:
-        transaction = db.graph.begin()
-        for archivable_tweet in (
-            Tweet.match(db.graph)
-            .where('NOT _:Archived')
-            .order_by('_.created_at DESC')
-            .skip(max_unarchived_tweets)
-        ):
-            archivable_tweet.archived = True
-            transaction.push(archivable_tweet)
-        transaction.commit()
-        time.sleep(archiving_interval)
+    for record in db.graph.run(
+        'MATCH (t:Tweet) '
+        'WHERE NOT t:Archived '
+        'RETURN t '
+        'ORDER BY t.created_at DESC '
+        'SKIP $max_unarchived_tweets',
+        max_unarchived_tweets=max_unarchived_tweets
+    ):
+        archivable_tweet = Tweet()
+        archivable_tweet.id = record['t']['id']
+        archivable_tweet.archived = True
+        db.graph.push(archivable_tweet)
+
+
+@celery_app.task
+def monitor_tweet_count_variation():
+    time_window_length_in_minutes = (
+        current_app.config['TIME_WINDOW_LENGTH_IN_MINUTES']
+    )
+    time_window_interval_in_minutes = (
+        current_app.config['TIME_WINDOW_INTERVAL_IN_MINUTES']
+    )
+    warning_threshold_difference = (
+        current_app.config['WARNING_THRESHOLD_DIFFERENCE']
+    )
+    # while True:
+    difference = (
+        get_tweet_count_percent_difference_for_subsequent_time_windows(
+            time_window_length_in_minutes, time_window_interval_in_minutes
+        )
+    )
+    if difference >= warning_threshold_difference:
+        capture_message(
+            f'Increase by {difference}% tweets since last time window.'
+        )
+    elif difference <= -warning_threshold_difference:
+        capture_message(
+            f'Reduction by {difference}% tweets since last time window.'
+        )
